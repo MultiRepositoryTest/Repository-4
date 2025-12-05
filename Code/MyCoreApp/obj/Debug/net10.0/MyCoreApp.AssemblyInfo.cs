e362cc17410e5a4dbffdefb40189350c550adf99
PFS-Horie0107 <hhorie@proface-sys.com>
2025-12-05 17:06:04 +0900

    commit

--- Code/MyCoreApp/obj/Debug/net10.0/MyCoreApp.AssemblyInfo.cs
+++ Code/MyCoreApp/obj/Debug/net10.0/MyCoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aed6e54c0f2dff1d907cb31c037984a578394484")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c20a3517c9c380baaf1b5d2e15bea4ca57b7f54")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
