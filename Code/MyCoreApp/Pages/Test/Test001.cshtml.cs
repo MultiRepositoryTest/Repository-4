7c20a3517c9c380baaf1b5d2e15bea4ca57b7f54
PFS-Horie0107 <hhorie@proface-sys.com>
2025-12-05 17:04:50 +0900

    Commit

---
+++ Code/MyCoreApp/Pages/Test/Test001.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyCoreApp.Pages.Test;
+
+public class Test001Model : PageModel
+{
+    public void OnGet()
+    {
+    }
+    public string TextBox { get; set; }
+    public bool CheckBox { get; set; }
+    public string RadioButton { get; set; }
+    public string SelectBox { get; set; }
+    public string TextArea { get; set; }
+    public Test001Model()
+    { 
+        TextBox=""; 
+        RadioButton="";
+        SelectBox="";
+        TextArea="";
+    }
+
+}
+public enum RadioButton
+{ 選択肢１,選択肢２,選択肢３ }
+public enum SelectBox
+{ 選択肢１,選択肢２,選択肢３ }
